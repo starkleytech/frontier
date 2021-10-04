3231373c0fd84a3adb4a7fa107201390284b397b
dubs <julien.du.bois@starkleytech.com>
2021-10-04 16:38:45 +0400

    update fp-evm

--- primitives/evm/src/lib.rs
+++ primitives/evm/src/lib.rs
@@ -1,35 +1,16 @@
-// SPDX-License-Identifier: Apache-2.0
-// This file is part of Frontier.
-//
-// Copyright (c) 2020 Parity Technologies (UK) Ltd.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// 	http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #![cfg_attr(not(feature = "std"), no_std)]
 
 mod precompile;
-mod internal_transaction;
-pub use self::internal_transaction::{InternalTxDetails, RewardInfo, };
 
-use codec::{Encode, Decode};
-#[cfg(feature = "std")]
-use serde::{Serialize, Deserialize};
-use sp_std::vec::Vec;
-use sp_core::{U256, H160};
+use codec::{Decode, Encode};
 use evm::ExitReason;
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+use sp_core::{H160, U256};
+use sp_std::vec::Vec;
 
-pub use evm::backend::{Basic as Account, Log };
-pub use precompile::{Precompile, PrecompileSet, LinearCostPrecompile};
+pub use evm::backend::{Basic as Account, Log};
+pub use precompile::{LinearCostPrecompile, Precompile, PrecompileSet};
 
 #[derive(Clone, Eq, PartialEq, Encode, Decode, Default)]
 #[cfg_attr(feature = "std", derive(Debug, Serialize, Deserialize))]
@@ -48,7 +29,6 @@ pub struct ExecutionInfo<T> {
 	pub value: T,
 	pub used_gas: U256,
 	pub logs: Vec<Log>,
-	pub internal_txs: Vec<InternalTxDetails>,
 }
 
 pub type CallInfo = ExecutionInfo<Vec<u8>>;
